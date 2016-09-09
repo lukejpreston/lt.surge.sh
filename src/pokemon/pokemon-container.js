dd0d903111626c5d021168abe6e2cbb722a8f0f5
Luke Preston <luke.preston@bbc.co.uk>
2016-09-09 09:03:51 +0100

    some cleaner aligning

--- src/pokemon/pokemon-container.js
+++ src/pokemon/pokemon-container.js
@@ -7,13 +7,13 @@ import reducer from './pokemon-reducer'
 import dispatcher from './pokemon-dispatcher'
 
 let Pokemon = () => {
-  let list = null
-  let preview = null
-  // let list = <div className='list'>list</div>
-  // let preview = <div className='preview'>preview</div>
+  // let list = null
+  // let preview = null
+  let list = <div className='list'>list</div>
+  let preview = <div className='preview'>preview</div>
 
-  // let details = null
-  let details = <div className='details'>details</div>
+  let details = null
+  // let details = <div className='details'>details</div>
 
   return <div className='page'>
     {list}
