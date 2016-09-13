444d1bdbaa01fba55a2f18be289fac19815bf793
Luke Preston <luke.preston@bbc.co.uk>
2016-09-13 07:08:17 +0100

    previewed

--- src/pokemon/pokemon-preview-component.js
+++ src/pokemon/pokemon-preview-component.js
@@ -1,13 +1,22 @@
+import './pokemon-preview.css'
 import React, { PropTypes } from 'react'
 
 import { Link } from 'react-router'
 
 let Preview = ({preview}) => {
-  return <div className='preview'>
-    <img src={preview.image.src} alt={preview.image.alt} />
-    <span>{preview.name}</span>
-    <span>{preview.flavour}</span>
-    <Link to={preview.to}>MORE</Link>
+  return <div className='preview pokemon'>
+    <div className='img-container'>
+      <img src={preview.image.src} alt={preview.image.alt} />
+    </div>
+    <div className='title'>
+      <span>{preview.name}</span>
+    </div>
+    <div className='flavour'>
+      <span>{preview.flavour}</span>
+    </div>
+    <div className='button-container'>
+      <Link to={preview.to} className='button'>MORE</Link>
+    </div>
   </div>
 }
 
--- src/pokemon/pokemon-reducer.js
+++ src/pokemon/pokemon-reducer.js
@@ -7,8 +7,8 @@ export default reducer({
     preview: {
       to: '/pokemon/lt.surge',
       image: require('../sprites/thunder-stone.json'),
-      name: 'lt. Surge',
-      flavour: 'This is probably the best pokedex you will find'
+      name: 'LT. Surge',
+      flavour: 'LT. Surge knows everything there is to know about pokemon. He is so cool because he has a Raichu. What is a "Raicu"? Use the list to the side to search for that little thing! Already got one? You must be pretty damn good to have a Raichu! Then select the pokeball to indicate you have caught one and then you can filter it out by only searching for "Uncaught" pokemon. Now you don\'t need to see that oversized electric rat no more.'
     }
   },
   reduce (state, action) {
